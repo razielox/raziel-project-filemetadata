5257f4e1481781bb16e45c51270ffe8798e4b419
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-04-21 12:49:47 -0500

    update1

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'),(request, response) => {
+  //console.log(request.file, request.body  )
+  response.json({name: request.file.originalname, type: request.file.mimetype, size: request.file.size})
+})
 
 
 
